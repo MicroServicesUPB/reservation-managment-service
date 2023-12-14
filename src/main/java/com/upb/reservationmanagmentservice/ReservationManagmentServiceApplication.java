115ce5a75ec74a282924b5409054de02e4a5fee7
HugoAlejandro2002 <apazahuaychohugoalejandro@gmail.com>
2023-12-14 13:38:52 -0400

    first commit

---
+++ src/main/java/com/upb/reservationmanagmentservice/ReservationManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.upb.reservationmanagmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationManagmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationManagmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upb/reservationmanagmentservice/ReservationManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upb.reservationmanagmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
